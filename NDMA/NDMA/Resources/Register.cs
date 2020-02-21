7b55c3d5a9a112eb3d24202bce4790203d8ca924
WilliamCareySemiColon <wc16315253@gmail.com>
2020-02-21 10:41:45 +0000

    added the sex category spinner

--- NDMA/NDMA/Resources/Register.cs
+++ NDMA/NDMA/Resources/Register.cs
@@ -23,17 +23,20 @@ namespace NDMA.Resources
             // Create your application here
             SetContentView(Resource.Layout.Register);
 
+            //age category details
             Spinner ageCategory = FindViewById<Spinner>(Resource.Id.AgeCategory);
-            ageCategory.Prompt = "Age Category";
-            
             ageCategory.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(Spinner_ItemSelected);
-
             ArrayAdapter ageCategoryAdapter = ArrayAdapter.CreateFromResource(this, 
                 Resource.Array.AgeCategory, Android.Resource.Layout.SimpleSpinnerItem);
-
             ageCategoryAdapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
-
             ageCategory.Adapter = ageCategoryAdapter;
+            //sex category details
+            Spinner sexCategory = FindViewById<Spinner>(Resource.Id.SexCategory);
+            sexCategory.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(Spinner_ItemSelected);
+            ArrayAdapter sexCategoryAdpater = ArrayAdapter.CreateFromResource(this,
+                Resource.Array.SexCategory, Android.Resource.Layout.SimpleSpinnerItem);
+            sexCategoryAdpater.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+            sexCategory.Adapter = sexCategoryAdpater;
 
         }
 
