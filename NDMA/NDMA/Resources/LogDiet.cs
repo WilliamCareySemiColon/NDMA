c2a7de56a6c769366642a3c43d6eea81ed0afae5
WilliamCareySemiColon <wc16315253@gmail.com>
2020-02-23 17:32:30 +0000

    added the handlers to the buttons on the log function

--- NDMA/NDMA/Resources/LogDiet.cs
+++ NDMA/NDMA/Resources/LogDiet.cs
@@ -21,6 +21,27 @@ namespace NDMA.Resources
 
             // Create your application here
             SetContentView(Resource.Layout.LogDiet);
+
+            //Button items and their handlers
+            Button add = FindViewById<Button>(Resource.Id.Add);
+            Button cancel = FindViewById<Button>(Resource.Id.Cancel);
+            Button discard = FindViewById<Button>(Resource.Id.Discard);
+            Button save = FindViewById<Button>(Resource.Id.Save);
+            Button submit = FindViewById<Button>(Resource.Id.Submit);
+            //applying the handlers to the buttons
+            add.Click += delegate { ButtonClicked("add"); };
+            cancel.Click += delegate { ButtonClicked("cancel"); };
+            discard.Click += delegate { ButtonClicked("discard"); };
+            save.Click += delegate { ButtonClicked("save"); };
+            submit.Click += delegate { ButtonClicked("submit"); };
+        }
+
+        private void ButtonClicked(string id)
+        {
+            Toast.MakeText(Application.Context,
+                "You have pressed the button with the id " + id, ToastLength.Short).Show();
+
+
         }
     }
 }
