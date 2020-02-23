d77dbb77d252ae473e87524037cba0fbb197c6c5
WilliamCareySemiColon <wc16315253@gmail.com>
2020-02-23 15:05:41 +0000

    updated the home page and create the connections

--- NDMA/NDMA/Resources/Home.cs
+++ NDMA/NDMA/Resources/Home.cs
@@ -21,6 +21,22 @@ namespace NDMA.Resources
 
             // Create your application here
             SetContentView(Resource.Layout.Home);
+            //Button Connections
+            Button viewAccount = FindViewById<Button>(Resource.Id.ViewAccount);
+            Button vistAdvisorSystem = FindViewById<Button>(Resource.Id.AdvisorSystem);
+            Button LogDailyDiet = FindViewById<Button>(Resource.Id.LogDiet);
+
+            viewAccount.Click += delegate { ButtonClicked("View"); };
+
+            vistAdvisorSystem.Click += delegate { ButtonClicked("Advise"); };
+
+            LogDailyDiet.Click += delegate { ButtonClicked("Log"); };
+        }
+
+        private void ButtonClicked(string id)
+        {
+            Toast.MakeText(Application.Context, "Ypu have pressed the button with the id of " + id,
+                ToastLength.Short).Show();
         }
     }
 }
