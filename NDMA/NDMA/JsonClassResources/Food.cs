24257858302b6d1daf63d4fc98090e4cc07c3ffb
WilliamCareySemiColon <wc16315253@gmail.com>
2020-02-26 14:12:10 +0000

    added the start of the rest api into the application

---
+++ NDMA/NDMA/JsonClassResources/Food.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace NDMA
+{
+    public class Food
+    {
+        public String Text { get; set; }
+
+        //public List<String> Parsed { get; set; }
+
+        //public String Hints { get; set; }
+
+
+    }
+}
--- NDMA/NDMA/Resources/LoggingFood/SearchForFood.cs
+++ NDMA/NDMA/Resources/LoggingFood/SearchForFood.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 
 using Android.App;
@@ -9,12 +10,19 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Newtonsoft.Json;
 
 namespace NDMA.Resources.LoggingFood
 {
     [Activity(Label = "SearchForFood")]
     public class SearchForFood : Activity
     {
+        private readonly string[] FoodDBApiCreds = new string[]
+        {
+            "40bde6df" ,
+            "3249bb41449954869d9cae17f11061b1"
+        };
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -23,24 +31,62 @@ namespace NDMA.Resources.LoggingFood
             SetContentView(Resource.Layout.SearchForFood);
 
             EditText search = FindViewById<EditText>(Resource.Id.Search);
-            search.TextChanged += delegate
-            {
-                TextChanged(search.Text);
-            };
 
             //Button handlers
             Button cancel = FindViewById<Button>(Resource.Id.Cancel);
+            Button searchBtn = FindViewById<Button>(Resource.Id.btnSearch);
 
             cancel.Click += delegate { Finish(); };
-            //getting to the 
+            searchBtn.Click += delegate
+            {
+                TextChanged(search.Text);
+            };
 
         }
 
         private void TextChanged(string message)
         {
-            TextView text = FindViewById<TextView>(Resource.Id.SampleTest);
+            //TextView text = FindViewById<TextView>(Resource.Id.SampleTest);
 
-            text.Text = message;
+            if(message.Length >= 3)
+            {
+                GetFood(message);
+            }
         }
+
+        private async void GetFood(String keyWord)
+        {
+            HttpClient client = new HttpClient();
+            string url = $"https://api.edamam.com/api/food-database/parser?ingr={keyWord}&app_id={FoodDBApiCreds[0]}&app_key={FoodDBApiCreds[1]}";
+            HttpResponseMessage response;
+            String json;
+            Uri uri;
+            Food food;
+            try
+            {
+                uri = new Uri(url);
+                response = await client.GetAsync(uri);
+                json = await response.Content.ReadAsStringAsync();
+                //Toast.MakeText(Application.Context, json.Length, ToastLength.Long).Show();
+                food = JsonConvert.DeserializeObject<Food>(json);
+
+                Toast.MakeText(Application.Context, food.Text, ToastLength.Short).Show();
+
+                //Console.WriteLine(food.Text);
+            }
+            catch(Exception e)
+            {
+                Toast.MakeText(Application.Context,"Exception " +  e.Message.ToString(), ToastLength.Long).Show();
+            }
+            //var response = await client.GetAsync(url).ConfigureAwait(false);
+            
+            
+            //Toast.MakeText(Application.Context, "Before the read items as string ", ToastLength.Long).Show();
+            //var json = await response.Content.ReadAsStringAsync();
+
+            //Toast.MakeText(Application.Context,json.Length, ToastLength.Long).Show();
+        }
+
+            
     }
 }
