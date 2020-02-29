0d394c1788702ad3e3827dc3beb7be5ce0b1ead7
WilliamCareySemiColon <wc16315253@gmail.com>
2020-02-29 16:33:06 +0000

    remeoved redundant code from the api class

--- NDMA/NDMA/Resources/Activitites/SearchForFoodFromApi.cs
+++ NDMA/NDMA/Resources/Activitites/SearchForFoodFromApi.cs
@@ -33,6 +33,13 @@ namespace NDMA.Resources
 
         string[] ListToDisplayTemp;
 
+        //The variable to work with in this class
+        HttpClient client;
+        HttpResponseMessage response;
+        String json;
+        Uri uri;
+        ParsedFoodCollection food;
+        CustomSearchedAPIListAdapter listAdapter;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -54,23 +61,16 @@ namespace NDMA.Resources
         private void SearchApi(string message)
         {
             if (message.Length >= 3)
-            {
                 GetFood(message);
-            }
         }
 
         private async void GetFood(String keyWord)
         {
-            HttpClient client = new HttpClient();
+            client = new HttpClient();
             int from = 0, to = 10;
             //string url = $"https://api.edamam.com/api/food-database/parser?ingr={keyWord}&app_id={FoodDBApiCreds[0]}&app_key={FoodDBApiCreds[1]}";
             string url = "https://api.edamam.com/search?q=" + keyWord + "&app_id=" + RecipeSearchApCreds[0] + "&app_key="
                 + RecipeSearchApCreds[1] + "&from=" + from + "&to=" + to;
-            HttpResponseMessage response;
-            String json;
-            Uri uri;
-            ParsedFoodCollection food;
-            CustomSearchedAPIListAdapter listAdapter;
             ListView list = FindViewById<ListView>(Resource.Id.SearchFoodList);
             try
             {
@@ -82,37 +82,14 @@ namespace NDMA.Resources
                 Toast.MakeText(Application.Context, food.Q + " " + food.Count + " " + food.Hits.ToArray()[1].Recipe.label
                     , ToastLength.Short).Show();
 
-                //attempting to connect to the listview
-                int Size = food.Hits.Count;
-                ListToDisplayTemp = new string[Size];
-
-                for (int i = 0; i < Size; i++)
-                {
-                    ListToDisplayTemp[i] = food.Hits.ToArray()[i].Recipe.label;
-                }
-
-                 listAdapter = new CustomSearchedAPIListAdapter(this, food);
+                listAdapter = new CustomSearchedAPIListAdapter(this, food);
                 list.Adapter = listAdapter;
-
-                
             }
             catch (Exception e)
             {
-                Toast.MakeText(Application.Context, "Exception " + e.Message.ToString(), ToastLength.Long).Show();
-
-                //attempting to connect to the listview
-                int Size = 10;
-                ListToDisplayTemp = new string[Size];
-
-                for (int i = 0; i < Size; i++)
-                {
-                    ListToDisplayTemp[i] = "Apple";
-                }
-                ArrayAdapter listAdapter2 = new ArrayAdapter(this,
-                    Android.Resource.Layout.SimpleExpandableListItem1,
-                    ListToDisplayTemp);
-
-                list.Adapter = listAdapter2;
+                Toast.MakeText(Application.Context,
+                    "Error while retreiving the results from the api. Try agina later"
+                    + e.Message.ToString(), ToastLength.Long).Show();
             }
 
             //{ keyWord};
