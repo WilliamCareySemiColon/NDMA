5fd0d2c9af5acdddd6b7f15d6694f2a2d8c1f101
WilliamCareySemiColon <wc16315253@gmail.com>
2020-03-06 11:08:02 +0000

    added the new test data and removed redundant files

--- NDMA/NDMA/Resources/Activitites/LogDiet.cs
+++ NDMA/NDMA/Resources/Activitites/LogDiet.cs
@@ -10,7 +10,6 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using NDMA.Resources.Adapter;
-using NDMA.Resources.LoggingFood;
 
 namespace NDMA.Resources
 {
---
+++ NDMA/NDMA/Resources/ZZZTestData/TestSampleData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace NDMA.Resources.ZZZTestData
+{
+    public static class TestSampleData
+    {
+        public static string Name = "William";
+        public static string Username = "Wiilo190198";
+        public static string Password = "password";
+        public static string Email = "abc@gmail.com";
+        public static string SexCategory = "Male";
+        public static string AgeCategory = "Between 18 and 35";
+        public static string Diet = null;
+        public static string NutritionDefincy = null;
+    }
+}
